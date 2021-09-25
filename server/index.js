f38a1150b3f50532e8f9ca980a0f139300f160d2
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-09-25 18:38:13 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // server/index.js
 const express = require("express");
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.get("/api", (req, res) => {
   res.json({ message: "lol" });
